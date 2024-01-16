91df68ffa9e5bb1b6efe88f083b983fb3eb224fa
sujitHirapara <99823787+sujitHirapara@users.noreply.github.com>
2024-01-16 22:57:01 +0530

    Home page changes updated

--- public/js/app.js
+++ public/js/app.js
@@ -5727,6 +5727,35 @@ jquery__WEBPACK_IMPORTED_MODULE_0___default()('.clients-slider').slick({
   }]
 });
 
+// Featured Carousel
+jquery__WEBPACK_IMPORTED_MODULE_0___default()('.featured-carousel').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  arrows: true,
+  dots: false,
+  responsive: [{
+    breakpoint: 1024,
+    settings: {
+      slidesToShow: 3,
+      slidesToScroll: 1
+    }
+  }, {
+    breakpoint: 767,
+    settings: {
+      slidesToShow: 2,
+      slidesToScroll: 1
+    }
+  }, {
+    breakpoint: 576,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1
+    }
+  }]
+}).on('setPosition', function (event, slick) {
+  slick.$slides.css('height', slick.$slideTrack.height() + 'px');
+});
+
 // Product Slider
 jquery__WEBPACK_IMPORTED_MODULE_0___default()('.product-slider').slick({
   slidesToShow: 1,
--- src/js/app.js
+++ src/js/app.js
@@ -68,6 +68,39 @@ $('.clients-slider').slick({
     ]
 });
 
+// Featured Carousel
+$('.featured-carousel').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: false,
+    responsive: [
+    {
+        breakpoint: 1024,
+        settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        }
+    },
+    {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+    },
+    {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+    },
+    ]
+}).on('setPosition', function (event, slick) {
+    slick.$slides.css('height', slick.$slideTrack.height() + 'px');
+});
+
 // Product Slider
 $('.product-slider').slick({
     slidesToShow: 1,
